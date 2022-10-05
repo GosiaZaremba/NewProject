43bb50ae5e435a8e734f2427912cc54ea6786531
Gosia <gosia@szel.net>
2022-10-05 14:45:46 +0200

    Added new stories.

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -6,11 +6,21 @@
 function loadStories() {
     require('../components/atoms/CustomButton/CustomButton.stories');
     require('../components/atoms/CustomIcon/CustomIcon.stories');
+    require('../components/atoms/CustomInput/CustomInput.stories');
+    require('../components/atoms/CustomSwitch/CustomSwitch.stories');
+    require('../components/atoms/Divider/Divider.stories');
+    require('../components/atoms/Label/Label.stories');
+    require('../components/atoms/Link/Link.stories');
 }
 
 const stories = [
     '../components/atoms/CustomButton/CustomButton.stories',
     '../components/atoms/CustomIcon/CustomIcon.stories',
+    '../components/atoms/CustomInput/CustomInput.stories',
+    '../components/atoms/CustomSwitch/CustomSwitch.stories',
+    '../components/atoms/Divider/Divider.stories',
+    '../components/atoms/Label/Label.stories',
+    '../components/atoms/Link/Link.stories',
 ];
 
 module.exports = {
